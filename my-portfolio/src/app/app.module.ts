dbc0d6b4a18eea4861d59748d15896ab243f2c8f
luisnarv <115510650+luisnarv@users.noreply.github.com>
2023-10-27 12:06:16 -0500

    update

--- my-portfolio/src/app/app.module.ts
+++ my-portfolio/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -26,7 +28,9 @@ import { FooterComponent } from './components/footer/footer.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- my-portfolio/src/app/components/footer/footer.component.ts
+++ my-portfolio/src/app/components/footer/footer.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+import { debounceTime } from 'rxjs';
+debounceTime
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
+
+
+
+  constructor() { }
+
+  ngOnInit(): void {}
+
+get name(){
+  return this.form.get('name') as FormControl;
+}
+get email(){
+  return this.form.get('email') as FormControl;
+}
+get message(){
+  return this.form.get('message') as FormControl;
+}
+
+
+  form = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required]),
+    message: new FormControl('', [Validators.required]),
+  })
+
+  
 
 }
+
